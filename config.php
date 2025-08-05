0c82468acd46085ec681e0cc52d489642ba2d5e8
Colin Fitz-Maurice <colin@fitz-maurice.com>
2025-08-04 17:09:01 -0700

    re-init

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => '',
+    'title' => 'Colin Fitz-Maurice',
+    'description' => 'Colin Fitz-Maurice is a software engineer and entrepreneur.',
+    'collections' => [],
+];
