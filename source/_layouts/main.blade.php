2b002f7be5a9243c789927e3bc7f9997e6ddb669
Colin Fitz-Maurice <colin@fitz-maurice.com>
2025-08-08 11:50:37 -0700

    icons and 404 page

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="{{ $page->language ?? 'en' }}">
+<html lang="{{ $page->language ?? 'en' }}" >
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
         <link rel="canonical" href="{{ $page->getUrl() }}">
         <meta name="description" content="{{ $page->description }}">
         <title>{{ $page->title }}</title>
@@ -10,7 +10,7 @@
         <link rel="stylesheet" href="{{ vite('source/_assets/css/main.css') }}">
         {{-- <script defer type="module" src="{{ vite('source/_assets/js/main.js') }}"></script> --}}
     </head>
-    <body class="text-gray-900 font-sans antialiased">
+    <body class="text-gray-900 font-sans antialiased leading-tight">
         @yield('body')
     </body>
 </html>
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,5 +1,7 @@
 @extends('_layouts.main')
 
+@section('title', 'Colin Fitz-Maurice')
+
 @section('body')
 <div class="p-8">
     <h1 class="text-3xl font-bold">Hello world!</h1>
